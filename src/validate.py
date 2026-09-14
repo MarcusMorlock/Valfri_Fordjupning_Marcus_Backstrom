c17b9b8ffac8eb262d3fe60ca9dcbf4de2c7a66f
Marcus Backstrom <VBMarcusVB@hotmail.com>
2026-09-14 15:45:28 +0200

    oops

--- src/validate.py
+++ src/validate.py
@@ -7,5 +7,10 @@ import pandas as pd
 from pathlib import Path
 from src import LOGGER_NAME, load_json, save_csv
 
-
+#   {
+#     "sensor_id": "S1",
+#     "region": "Orebro",
+#     "sector": "W",
+#     "temperature": 20.0
+#   },
 
